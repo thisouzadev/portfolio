be91beacfceb6f81df28bac1abd1c78fdb8d4aff
thiago <thiagodesouza.dev@gmail.com>
2022-07-13 19:34:57 -0300

    feat: :art: react animated hamburger menu

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable require-jsdoc */
-import React from 'react';
+import React, {useState} from 'react';
 import Contact from './components/contact/Contact';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
@@ -10,9 +10,10 @@ import Works from './components/works/Works';
 import './app.scss';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- my-app/src/components/topbar/Topbar.jsx
+++ my-app/src/components/topbar/Topbar.jsx
@@ -5,12 +5,12 @@ import './topbar.scss';
 import * as BSIcons from 'react-icons/bs';
 import * as MDIcons from 'react-icons/md';
 
-function Topbar() {
+function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo'>genius.</a>
+          <a href="#intro" className='logo'>Souza.</a>
           <div className="itemContainer">
             <BSIcons.BsPersonFill className='icon' />
             <span>+55 21979447656</span>
@@ -20,7 +20,13 @@ function Topbar() {
             <span>thiagodesouza.dev@gmail.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
       </div>
 
     </div>
