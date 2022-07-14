5e395d94fe1d71bb530f5020eaff15b2cbae3693
thiago <thiagodesouza.dev@gmail.com>
2022-07-13 23:14:54 -0300

    feat: :art: react text typing animation

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -1,11 +1,49 @@
 /* eslint-disable require-jsdoc */
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './intro.scss';
+import {init} from 'ityped';
 
-function Intro() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        'Reactjs',
+        'Nodejs',
+        'MongoDB',
+        'MySQL',
+        'Express',
+        'Sass',
+        'HTML5',
+        'CSS3',
+        'Javascript',
+        'Typescript'],
+    });
+  }, []);
+
   return (
-    <div className='intro' id='intro'>Intro</div>
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Olá, Eu Sou</h2>
+          <h1>Thiago Souza</h1>
+          <h3>
+            Desenvolvedor Web <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
   );
 }
-
-export default Intro;
