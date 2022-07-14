39b77ac7270d3e0034bd745c5ff2adf1e93f43f4
thiago <thiagodesouza.dev@gmail.com>
2022-07-14 11:39:57 -0300

    feat: :art: react portfolio page

--- my-app/src/components/portfolio/Portfolio.jsx
+++ my-app/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,74 @@
 /* eslint-disable require-jsdoc */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './portfolio.scss';
-
+import PortfolioList from '../portfolioList/PortfolioList';
+import {
+  frontPortfolio,
+  backPortfolio,
+  pythonPortfolio,
+} from '../../data';
 function Portfolio() {
+  const [selected, setSelected] = useState('front end');
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: 'front end',
+      title: 'front end',
+    },
+    {
+      id: 'back end',
+      title: 'back end',
+    },
+    {
+      id: 'python',
+      title: 'ciencia da computação',
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case 'front end':
+        setData(frontPortfolio);
+        break;
+      case 'back end':
+        setData(backPortfolio);
+        break;
+      case 'python':
+        setData(pythonPortfolio);
+        break;
+      default:
+        setData(frontPortfolio);
+    }
+  }, [selected]);
+
   return (
-    <div className='portfolio' id='portfolio'>Portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            key={item.id}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((data) => (
+          <a key={data.id} href={data.link} target="_blank" rel="noreferrer">
+            <div className="item" >
+              <img
+                src={data.img}
+                alt=""
+              />
+              <h3>{data.title}</h3>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
   );
 }
 
---
+++ my-app/src/data.js
@@ -0,0 +1,113 @@
+export const frontPortfolio = [
+  {
+    id: 1,
+    title: 'projeto Trybewarts',
+    img:
+      'assets/trybewarts.png',
+    link: 'https://github.com/thisouzadev/fundamentos-projeto-trybewarts',
+  },
+
+  {
+    id: 2,
+    title: 'Todo list',
+    img:
+      'assets/todo-list.png',
+    link: 'https://github.com/thisouzadev/fundamentos-todo-list',
+  },
+  {
+    id: 3,
+    title: ' Movie Cards CRUD',
+    img: 'assets/movie-cards-crud.png',
+    link: 'https://github.com/thisouzadev/front-end-movie-card-library-crud',
+  },
+  {
+    id: 4,
+    title: 'Trivia!',
+    img: 'assets/trivia.png',
+    link: 'https://github.com/thisouzadev/front-end-trivia-react-redux',
+  },
+  {
+    id: 5,
+    title: 'Starwars Planets Search',
+    img: 'assets/starwars-planets-search.png',
+    link: 'https://github.com/thisouzadev/front-end-starwars-planets-search',
+  },
+  {
+    id: 6,
+    title: 'projeto de Receitas!',
+    img: 'assets/receitas.png',
+    link: 'https://github.com/thisouzadev/front-end-project-recipes-app',
+  },
+];
+
+export const backPortfolio = [
+  {
+    id: 1,
+    title: 'projeto Cookmaster!',
+    img: 'assets/chief-cook.png',
+    link: 'https://github.com/thisouzadev/back-end-cookmaster',
+  },
+  {
+    id: 2,
+    title: 'API de Blogs!',
+    img: 'assets/api-blogs.png',
+    link: 'https://github.com/thisouzadev/back-end-blogs-api',
+  },
+  {
+    id: 3,
+    title: 'Talker Manager!',
+    img: 'assets/talker-manager.png',
+    link: 'https://github.com/thisouzadev/back-end-project-talker-manager',
+  },
+  {
+    id: 4,
+    title: 'Aggregations!',
+    img: 'assets/mongodb.png',
+    link: 'https://github.com/thisouzadev/back-end-mongodb-aggregations',
+  },
+  {
+    id: 5,
+    title: 'projeto One For All!',
+    img: 'assets/mysql.png',
+    link: 'https://github.com/thisouzadev/back-end-mysql-one-for-all',
+  },
+];
+
+export const pythonPortfolio = [
+  {
+    id: 1,
+    title: 'Tech News!',
+    img: 'assets/tech-news.png',
+    link: 'https://github.com/thisouzadev/ciencia-da-computacao-tech-news',
+  },
+  {
+    id: 2,
+    title: 'Restaurant Orders!',
+    img: 'assets/restaurant-orders.png',
+    link: 'https://github.com/thisouzadev/ciencia-da-computacao-restaurant-orders',
+  },
+  {
+    id: 3,
+    title: 'TING(Trybe is not Google)!',
+    img: 'assets/ting.png',
+    link: 'https://github.com/thisouzadev/-ciencia-da-computacao-project-ting',
+  },
+  {
+    id: 4,
+    title: 'Job Insights!',
+    img: 'assets/python.png',
+    link: 'https://github.com/thisouzadev/ciencia-da-computacao-job-insights',
+  },
+  {
+    id: 5,
+    title: 'projeto de Algorithms!',
+    img: 'assets/python.png',
+    link: 'https://github.com/thisouzadev/ciencia-da-computacao-project-algorithms',
+  },
+  {
+    id: 6,
+    title: 'Relatório de Estoque!',
+    img: 'assets/estoque.png',
+    link: 'https://github.com/thisouzadev/ciencia-da-computacao-inventory-report',
+  },
+];
