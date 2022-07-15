4f2ae5672f16403de24dbc7da0f24c727e8335b8
thiago <thiagodesouza.dev@gmail.com>
2022-07-14 21:45:16 -0300

    feat: :art: react menu and intro photo update

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -29,7 +29,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
+          <img src="assets/a.jpeg" alt="" />
         </div>
       </div>
       <div className="right">
